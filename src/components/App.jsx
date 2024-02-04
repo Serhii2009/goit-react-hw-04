3641a3cdf5ecd09a0580bd5ab0908672304f2136
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-02-05 00:16:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,24 @@
 // import axios from "axios";
 // import getPictures from "./getPictures";
 
-// export const App = () => {
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const [pictures, setPictures] = useState([]);
-
-//   useEffect(() => {
-//     if (!searchQuery) return;
-
-//     const startFetching = async () => {
-//       setLoading(true);
-
-//       try {
-//         const { hits, totalHits } = await getPictures(searchQuery, page);
-//         setPictures((prevPictures) =>
-//           page === 1 ? hits : [...prevPictures, ...hits]
-//         );
-//       } catch (error) {
-//         setError(error.massage);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//   });
-// };
+export const App = () => {
+  // const [searchQuery, setSearchQuery] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // const [pictures, setPictures] = useState([]);
+  // useEffect(() => {
+  //   if (!searchQuery) return;
+  //   const startFetching = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const { hits, totalHits } = await getPictures(searchQuery, page);
+  //       setPictures((prevPictures) =>
+  //         page === 1 ? hits : [...prevPictures, ...hits]
+  //       );
+  //     } catch (error) {
+  //       setError(error.massage);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  // });
+};
